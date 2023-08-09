a1c13fcd359093ffc03215c387f5271991c26416
dbadiger <140099027+dbadiger@users.noreply.github.com>
2023-08-09 07:31:03 +0530

    Update ExpressJS.js

--- ExpressJS.js
+++ ExpressJS.js
@@ -13,4 +13,12 @@ app.get("/contact",(request, response)=>{
   response.send("Welcome, You are in Contact Us Page");
 });
 
+//display query in html
+// https://localhost:4000/?name=alpha
+app.get('', (req, resp)=>{
+ console.log("data received:",req.query); // this will display output in object format like: {name:alpha}
+ console.log("data received:",req.query.name); // this will display required output(displays only name)
+ resp.send("Welcome t Home page,"+req.query.name);
+});
+
 app.listen(4000);
