d120e7dde797d29d5b1ac5bbe682ad75a816a752
dbadiger <140099027+dbadiger@users.noreply.github.com>
2023-08-09 07:14:48 +0530

    Basic program for handling request and response

--- ExpressJS.js
+++ ExpressJS.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
 
+app.get('', (request, response)=>{
+ response.send("Hello, This is Home Page"); 
+});
+
+app.get("/about",(request, response)=>{
+  response.send("Welcome to about Us Page");
+});
+
+app.get("/contact",(request, response)=>{
+  response.send("Welcome, You are in Contact Us Page");
+});
+
+app.listen(4000);
