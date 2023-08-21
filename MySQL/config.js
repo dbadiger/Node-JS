9a191aaf8bc7be4fcfc4267a1c42888ada027d6a
dbadiger <140099027+dbadiger@users.noreply.github.com>
2023-08-21 10:12:35 +0530

    Update config.js

--- MySQL/config.js
+++ MySQL/config.js
@@ -1,1 +1,13 @@
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+  host:'localhost',          //you can give your online URL
+  user:'root',              //username
+  password:"",              //password
+  database:"mytest"        //database name
+});
+conn.connect(err)=>{
+  if(err)  console.log("Error in Connection")
+  else    console.log("Connected");
+}
 
+module.exports = conn;
